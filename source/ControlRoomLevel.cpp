d8e9f8884eaa2b0aef8baf43361fdf9daf57aa35
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2025-03-05 18:28:19 +0000

    change port

--- source/ControlRoomLevel.cpp
+++ source/ControlRoomLevel.cpp
@@ -51,7 +51,7 @@ void ControlRoomLevel::networkInit() {
     struct hostent* myhost = gethostbyname(keypad.getIP().data());
     sock = socket(AF_INET, SOCK_STREAM, 0);
     sain.sin_family = AF_INET;
-    sain.sin_port = htons(8080);
+    sain.sin_port = htons(25565);
     sain.sin_addr.s_addr = *((unsigned long*)(myhost->h_addr_list[0]));
     connect(sock, (struct sockaddr*)&sain, sizeof(sain));
 }
