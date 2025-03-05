d0ca000934bd7d9a955387e7f12ae90d5df0d99a
Harry Ingham <98830464+No-1-Goblin@users.noreply.github.com>
2025-03-05 17:11:19 +0000

    added sprite id

--- source/IPKeypad.cpp
+++ source/IPKeypad.cpp
@@ -1,8 +1,8 @@
 #include "IPKeypad.h"
 
-void IPKeyPad::load(int spriteRamId, int palRamId, int spriteVramId, int palVramId, int screen, Vector2f pos) {
+void IPKeyPad::load(int spriteRamId, int palRamId, int spriteVramId, int palVramId, int startSpriteId, int screen, Vector2f pos) {
     position = pos;
-    
+
 }
 
 void IPKeyPad::unload() {
--- source/IPKeypad.h
+++ source/IPKeypad.h
@@ -5,7 +5,7 @@
 
 class IPKeyPad {
     public:
-        void load(int spriteRamId, int palRamId, int spriteVramId, int palVramId, int screen, Vector2f pos);
+        void load(int spriteRamId, int palRamId, int spriteVramId, int palVramId, int startSpriteId, int screen, Vector2f pos);
         void unload();
     protected:
         Vector2f position;
